9913e620c66f04aa6510b75820be2a66adacb68f
Morgan <morgan.foucaut@greta-formations.fr>
2023-10-11 22:43:19 +0200

    Updated navbar css, added mentions in footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,10 +1,16 @@
-import styles from './footer.module.css'
+import style from './footer.module.css'
 
 
 export default function Footer() {
     return (
-        <footer className = { styles.footer }>
-            PVP IVS - Cylb - 2023
-        </footer>
+        <footer className = { style.footer }>
+            <p>PVP IVS - Cylb - 2023</p>
+                <div className={ style. mention }>
+                    <p>DNA icon made by <a href="https://www.flaticon.com/authors/maan-icons">maan-icons</a> from <a href="www.flaticon.com">www.flaticon.com</a></p>
+                    <p>Pokedex icon made by <a href="https://www.flaticon.com/fr/auteurs/roundicons-freebies">Roundicons Freebies</a> from <a href="www.flaticon.com">www.flaticon.com</a></p>
+                    <p>Data from <a href="https://pogoapi.net/">pogoapi.net</a></p>
+                    <p>Using Sprites from <a href="https://github.com/PokeMiners/pogo_assets">pogo_assets</a> Github repository</p>
+                </div>
+            </footer>
     )
 }
